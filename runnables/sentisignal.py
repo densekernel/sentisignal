e128e23e21062766ea9c0b93b7a555590e5ebf61
jonnymanf <me@jonnymanfield.com>
2016-02-28 20:25:45 +0000

    extend library, update notebooks, create new notebook for plotting mutual information surplus

--- runnables/sentisignal.py
+++ runnables/sentisignal.py
@@ -7,6 +7,7 @@ import matplotlib.dates as mdates
 import math
 import scipy.stats as s
 import statsmodels.api as sm
+import pprint
 
 from sklearn import metrics
 from sklearn.cluster import KMeans
@@ -16,6 +17,8 @@ from datetime import datetime
 from yahoo_finance import Share
 from pandas_datareader import data, wb
 from statsmodels.graphics.api import qqplot
+from operator import itemgetter
+from decimal import *
 
 plt.style.use('ggplot')
 
@@ -126,10 +129,12 @@ def preprocess_data_sentiment(df):
     df['LOG_BULL_RETURN'] = np.log(1+df['BULL_SCORED_MESSAGES']).diff()
     # log bear messages
     df['LOG_BEAR_RETURN'] = np.log(1+df['BEAR_SCORED_MESSAGES']).diff()
-    # log bullishness
+    # log bullishness (bull - bear volume)
     df['LOG_BULLISHNESS'] = np.log(1 + df['BULL_SCORED_MESSAGES']) - np.log(1 + df['BEAR_SCORED_MESSAGES'])
     # log bull bear ratio
     df['LOG_BULL_BEAR_RATIO'] = np.log(df['BULL_SCORED_MESSAGES']) / np.log(df['BEAR_SCORED_MESSAGES'])
+    # log bullishness bearishness change
+    df['LOG_BULL_MINUS_BEAR_CHANGE'] = np.log(1+df['BULL_MINUS_BEAR']).diff()
     # TISf
     df['TISf'] = (1+df['BULL_SCORED_MESSAGES'])/(1+ df['BULL_SCORED_MESSAGES']+df['BEAR_SCORED_MESSAGES'])
     # RTISf
@@ -222,6 +227,17 @@ def check_acf(df):
             plt.title(index)
     return fig
 
+# check adf test
+def adf_test(df):
+    df_num = df.select_dtypes(include=[np.float, np.int])
+    adf_test = pd.DataFrame(index = df_num.columns, columns = ['ADF'])
+    for i, column in enumerate(df_num.columns):
+        try:
+            adf_test.ix[i,0] = sm.tsa.stattools.adfuller(df[column][1:])
+        except:
+            print "errror"
+    return adf_test
+
 # apply rollling window to data
 def apply_rolling_window(df, width):
     df = df.ix[1:, :]
@@ -231,16 +247,19 @@ def apply_rolling_window(df, width):
     df_window = pd.concat([df_non_num, df_num_window], axis=1)
     return df_window
 
-def correlation_analysis(df):
-    i = 0
-    resCorr = pd.DataFrame(index = ['BULLISH_INTENSITY','BEARISH_INTENSITY','BULL_MINUS_BEAR','BULL_SCORED_MESSAGES',
-       'BEAR_SCORED_MESSAGES','LOG_BULLISHNESS','TISf','RTISf'] ,columns = ['LOG_RETURN','VOLUME','VOLATILITY'])
-    for column in resCorr.index:
-        resCorr.ix[i,0] = df['LOG_RETURN'].corr(df[column])
-        resCorr.ix[i,1] = df['VOLUME'].corr(df[column])
-        resCorr.ix[i,2] = df['VOLATILITY'].corr(df[column])
-        i += 1
-    return resCorr
+def correlation_analysis(df, threshold, data_sentiment, data_finance):
+    df_num = df.select_dtypes(include=[np.float, np.int])
+    res_corr = pd.DataFrame(index = df_num.columns, columns = df_num.columns)
+    corr_list = []
+    for i, col_i in enumerate(df_num.columns):
+        for j, col_j in enumerate(df_num.columns):
+            corr = df[col_i].corr(df[col_j])
+            res_corr.ix[i,j] = corr
+            if abs(corr) > threshold and abs(corr) < 0.99 and i < j and col_i in data_sentiment.columns and col_j in data_finance.columns:
+                corr_list.append([col_i, col_j, corr])
+    corr_list = list(reversed(sorted(corr_list, key=itemgetter(2))))
+    pprint.pprint(corr_list)
+    return res_corr
 
 # Sturges formula: k = log_2 n + 1  
 def sturges_bin(df):
@@ -254,25 +273,137 @@ def rice_bin(df):
 def doane_bin(data):
     n = data.count()
     std = np.std(data)
-    g = abs(s.moment(n,3)/(std**3))
-    u = math.sqrt(6*(n-1)/((n+1)*(n+3)))
-    return round(1 + np.log2(n) + np.log2(1+g/u))
+    g_1 = abs( s.moment(data,3) / s.moment(data, 2))
+    std_g_1 = Decimal(6 * (n - 2)) / Decimal( (n + 1) * (n + 2) )
+    std_g_1 = math.sqrt(std_g_1)
+    bins = round(1 + np.log2(n) + np.log2(1+g_1/std_g_1))
+    return bins
 
 def calc_mutual_information(x, y, bins):
+    if bins == -1:
+        bins = doane_bin(x)
     c_xy = np.histogram2d(x, y, bins)[0]
     mi = metrics.mutual_info_score(None, None, contingency=c_xy)
     return mi
 
-def information_surplus(df, time_shift, varx, vary, bins):
+def information_surplus(df, time_shift, varx, vary, bins, exante):
     output = []
-    for i in range(0, time_shift+1):
+    if exante:
+        shift_range = range(0, -(time_shift+1), -1)
+    else:
+        shift_range = range(0, time_shift+1)
+    for i in shift_range:
         shift_x = df[varx].shift(i)
-        x = shift_x.ix[1+i:]
-        y = df[vary].ix[1+i:]
+        if exante:
+            x = shift_x.ix[1:len(shift_x.index) - 1 - abs(i)]
+            y = df[vary].ix[1:len(shift_x.index) - 1 - abs(i)]
+        else:
+            x = shift_x.ix[1+abs(i):]
+            y = df[vary].ix[1+abs(i):]
         mi = calc_mutual_information(x, y, bins)
         if i == 0:
             mi_origin = mi
         output.append({'SHIFT': i, 'MUTUAL INFORMATION': mi, 'INFORMATION_SURPLUS_DIFF': mi - mi_origin, 'INFORMATION_SURPLUS_PCT': (mi - mi_origin) / mi_origin * 100})
     output_frame = pd.DataFrame(output)
     return output_frame
-                   
+
+# save mi results
+
+def save_information_surplus(dir_pickle, df, symbol, start_date, end_date, time_shift, varx, vary, bins, exante, window_size):
+    if window_size > 0: 
+        pickle_name = dir_pickle+'info_surp_res/'+symbol+'_'+start_date+'_'+end_date+'_'+str(window_size)+'_'+varx+'_'+vary+'_'+str(bins)+'_'+str(exante)+'.p' 
+    else:
+        pickle_name = dir_pickle+'info_surp_res/'+symbol+'_'+start_date+'_'+end_date+'_'+varx+'_'+vary+'_'+str(bins)+'_'+str(exante)+'.p'
+    df.to_pickle(pickle_name)
+    print("Saved to pickle: " + pickle_name)
+
+def load_information_surplus(dir_pickle, symbol, start_date, end_date, time_shift, varx, vary, bins, exante, window_size):
+    if window_size > 0: 
+        pickle_name = dir_pickle+'info_surp_res/'+symbol+'_'+start_date+'_'+end_date+'_'+str(window_size)+'_'+varx+'_'+vary+'_'+str(bins)+'_'+str(exante)+'.p' 
+    else:
+        pickle_name = dir_pickle+'info_surp_res/'+symbol+'_'+start_date+'_'+end_date+'_'+varx+'_'+vary+'_'+str(bins)+'_'+str(exante)+'.p'
+    print("Load pickle: " + pickle_name)
+    return pd.read_pickle(pickle_name)
+                
+def kmeans(df, features):
+    df_num = df.select_dtypes(include=[np.float, np.int])
+    # print df_num.info()
+    df_num = df_num[features]
+    # Convert DataFrame to matrix
+    mat = df_num.as_matrix()
+    # Using sklearn
+    km = KMeans()
+    km.fit(mat)
+    # Get cluster assignment labels
+    labels = km.labels_
+    # Format results as a DataFrame
+    # results = pandas.DataFrame([dataset.index,labels]).T
+
+    data = mat
+
+    reduced_data = PCA(n_components=2).fit_transform(data)
+    kmeans = KMeans(init='k-means++', n_clusters=6, n_init=10)
+    kmeans.fit(reduced_data)
+
+    # Step size of the mesh. Decrease to increase the quality of the VQ.
+    h = .02     # point in the mesh [x_min, m_max]x[y_min, y_max].
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
+    y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+    # Obtain labels for each point in mesh. Use last trained model.
+    Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure(1)
+    plt.clf()
+    plt.imshow(Z, interpolation='nearest',
+               extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+               cmap=plt.get_cmap('Set3'),
+               aspect='auto', origin='lower')
+
+    plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)
+    # Plot the centroids as a white X
+    centroids = kmeans.cluster_centers_
+    plt.scatter(centroids[:, 0], centroids[:, 1],
+                marker='x', s=169, linewidths=3,
+                color='w', zorder=10)
+    plt.title('K-means clustering on sentiment and finance metrics')
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+    plt.xticks(())
+    plt.yticks(())
+    plt.show()
+
+# graphical library function
+
+def plot_info_surplus(results, legend):
+
+    axes = plt.gca()
+    ymax = 0
+
+    for i, res in enumerate(results):
+        plt.plot(res['SHIFT'], res['INFORMATION_SURPLUS_PCT'])
+        ymax = max(res['INFORMATION_SURPLUS_PCT'].max(), ymax)
+
+    axes.set_ylim([0,ymax])
+    plt.legend(legend, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.xlabel('Time-shift of sentiment data (days) with financial data')
+    plt.ylabel('Information Surplus %')
+    plt.show()
+
+# sentisignal.plt.plot(a_sent_0['SHIFT'], a_sent_0['INFORMATION_SURPLUS_PCT'])
+# sentisignal.plt.plot(g_sent_0['SHIFT'], g_sent_0['INFORMATION_SURPLUS_PCT'])
+
+
+# ymin = min(a_sent_0['INFORMATION_SURPLUS_PCT'].min(), g_sent_0['INFORMATION_SURPLUS_PCT'].min())
+# ymin = 0
+# ymax = max(a_sent_0['INFORMATION_SURPLUS_PCT'].max(), g_sent_0['INFORMATION_SURPLUS_PCT'].max())
+
+
+# axes.set_xlim([xmin,xmax])
+
+
